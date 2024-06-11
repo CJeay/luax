65c4776394c48d69a55fb33b570cd41b86620643
Notably <135164982+CJeay@users.noreply.github.com>
2024-06-10 21:34:33 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,222 +1,203 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
-local follow = ".follow"
-getgenv().Following = nil
-local unfollow = ".unfollow"
-local die,reset = ".die", ".reset"
-local lock = ".locktoggle"
-local cmds = ".cmds"
-local plrs = game:GetService("Players")
-local plr = game:GetService("Players").LocalPlayer
-locked = false
-game:GetService("Players").PlayerAdded:Connect(function(player)
-    game.StarterGui:SetCore("SendNotification",  {
-        Title = "Player Joined";
-        Text = player.Name .. " Joined";
-        Icon = "";
-        Duration = 5;
-        Callback = NotificationBindable;
-    })
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Player Joined " .. player.Name ,"All")
-end)
-game:GetService("Players").PlayerRemoving:Connect(function(player)
-    game.StarterGui:SetCore("SendNotification",  {
-        Title = "Player Left";
-        Text = player.Name .. " Left";
-        Icon = "";
-        Duration = 5;
-        Callback = NotificationBindable;
-    })
-    game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Player Left " .. player.Name ,"All")
-end)
-GetArgs = function(Args)
-    return Args:split(" ")
-end
-FindPlayer = function(h,h2)
-    if string.lower(h) == "me" then
-        return plr
-    else
-        h = h:gsub("%s+", "")
-        for m, n in pairs(game:GetService("Players"):GetPlayers()) do
-            if n.Name:lower():match("^" .. h:lower()) or n.DisplayName:lower():match("^" .. h:lower()) then
-                return n
-            end
-        end
-    end
-    return nil
-end
---[[
-    local Args = GetArgs(message)
-    local r = FindPlayer(Args[2])
-    if r then
-        print(r)
-    else
-        print('nah')
-    end
-]]
-followingplr = nil
-for i,v in pairs(plrs:GetPlayers()) do
-    v.Chatted:Connect(function(message)
-	if not locked then
-        if string.find(message, follow) then
-            local Args = GetArgs(message)
-            local r = FindPlayer(Args[2])
-            if r then
-                followingplr = r
-            else
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "Could Not Find Player!";
-                    Text = "";
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                return
-            end
-            getgenv().Following = true
-            game.StarterGui:SetCore("SendNotification",  {
-                Title = "Now Following";
-                Text = followingplr.Name;
-                Icon = "";
-                Duration = 5;
-                Callback = NotificationBindable;
-            })
-            task.spawn(function()
-                while true do task.wait()
-                    if not getgenv().Following then
-                        break
-                    end
-                    plr.Character:WaitForChild("HumanoidRootPart").CFrame = followingplr.Character:WaitForChild("HumanoidRootPart").CFrame
-                end
-            end)
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("now following " .. followingplr.Name,"All")
-        end
-        if string.sub(string.lower(unfollow), 0, #message) == string.lower(message) or string.find(message,unfollow) then
-            if getgenv().Following then
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "No Longer Following";
-                    Text = followingplr.Name;
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                getgenv().Following = false
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("no longer following " .. followingplr.Name,"All")
-            else
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "Not Following Anyone";
-                    Text = "";
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("not following anyone","All")
-            end
-        end
-        if string.sub(string.lower(reset), 0, #message) == string.lower(message) or string.sub(string.lower(die), 0, #message) == string.lower(message) then
-            plr.Character.Humanoid.Health = 0
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("now dying","All")
-        end
-        if string.find(message,cmds) then
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(".follow [plr] .unf .die .reset","All")
-        end
-	if string.find(message,lock) then
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("bot has been locked to whitelist only","All")
-		locked = true
+game.StarterGui:SetCore("SendNotification", {
+	Title = "Fling Bot";
+	Text = "LMAO";
+	Duration = 3;
+})
+
+local Players = game:GetService('Players')
+
+local TextChatService = game:GetService('TextChatService')
+local ReplicatedStorage = game:GetService('ReplicatedStorage')
+
+local SkidFling = function(TargetPlayer)
+	local Character = game.Players.LocalPlayer.Character
+	local Humanoid = Character and Character:FindFirstChildOfClass("Humanoid")
+	local RootPart = Humanoid and Humanoid.RootPart
+
+	local TCharacter = TargetPlayer.Character
+	local THumanoid
+	local TRootPart
+	local THead
+	local Accessory
+	local Handle
+
+	if TCharacter:FindFirstChildOfClass("Humanoid") then
+		THumanoid = TCharacter:FindFirstChildOfClass("Humanoid")
 	end
+	if THumanoid and THumanoid.RootPart then
+		TRootPart = THumanoid.RootPart
+	end
+	if TCharacter:FindFirstChild("Head") then
+		THead = TCharacter.Head
+	end
+	if TCharacter:FindFirstChildOfClass("Accessory") then
+		Accessory = TCharacter:FindFirstChildOfClass("Accessory")
+	end
+	if Accessory and Accessory:FindFirstChild("Handle") then
+		Handle = Accessory.Handle
+	end
+
+	if Character and Humanoid and RootPart then
+		if RootPart.Velocity.Magnitude < 50 then
+			getgenv().OldPos = RootPart.CFrame
+		end
+		if THumanoid and THumanoid.Sit and not AllBool then
+
+		end
+		if THead then
+			workspace.CurrentCamera.CameraSubject = THead
+		elseif not THead and Handle then
+			workspace.CurrentCamera.CameraSubject = Handle
+		elseif THumanoid and TRootPart then
+			workspace.CurrentCamera.CameraSubject = THumanoid
+		end
+		if not TCharacter:FindFirstChildWhichIsA("BasePart") then
+			return
+		end
+
+		local FPos = function(BasePart, Pos, Ang)
+			RootPart.CFrame = CFrame.new(BasePart.Position) * Pos * Ang
+			Character:SetPrimaryPartCFrame(CFrame.new(BasePart.Position) * Pos * Ang)
+			RootPart.Velocity = Vector3.new(9e7, 9e7 * 10, 9e7)
+			RootPart.RotVelocity = Vector3.new(9e8, 9e8, 9e8)
+		end
+
+		local SFBasePart = function(BasePart)
+			local TimeToWait = 2
+			local Time = tick()
+			local Angle = 0
+
+			repeat
+				if RootPart and THumanoid then
+					if BasePart.Velocity.Magnitude < 50 then
+						Angle = Angle + 100
+
+						FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle),0 ,0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(2.25, 1.5, -2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(-2.25, -1.5, 2.25) + THumanoid.MoveDirection * BasePart.Velocity.Magnitude / 1.25, CFrame.Angles(math.rad(Angle), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, 1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, 0) + THumanoid.MoveDirection,CFrame.Angles(math.rad(Angle), 0, 0))
+						task.wait()
+					else
+						FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, -THumanoid.WalkSpeed), CFrame.Angles(0, 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, 1.5, THumanoid.WalkSpeed), CFrame.Angles(math.rad(90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, -TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(0, 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, 1.5, TRootPart.Velocity.Magnitude / 1.25), CFrame.Angles(math.rad(90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(math.rad(90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5 ,0), CFrame.Angles(math.rad(-90), 0, 0))
+						task.wait()
+
+						FPos(BasePart, CFrame.new(0, -1.5, 0), CFrame.Angles(0, 0, 0))
+						task.wait()
+					end
+				else
+					break
+				end
+			until BasePart.Velocity.Magnitude > 500 or BasePart.Parent ~= TargetPlayer.Character or TargetPlayer.Parent ~= Players or not TargetPlayer.Character == TCharacter or THumanoid.Sit or Humanoid.Health <= 0 or tick() > Time + TimeToWait
+		end
+
+		workspace.FallenPartsDestroyHeight = 0/0
+
+		local BV = Instance.new("BodyVelocity")
+		BV.Name = "EpixVel"
+		BV.Parent = RootPart
+		BV.Velocity = Vector3.new(9e8, 9e8, 9e8)
+		BV.MaxForce = Vector3.new(1/0, 1/0, 1/0)
+
+		Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, false)
+
+		if TRootPart and THead then
+			if (TRootPart.CFrame.p - THead.CFrame.p).Magnitude > 5 then
+				SFBasePart(THead)
 			else
-				if plr.Name == 'CJstickguy' then
-				if string.find(message, follow) then
-            local Args = GetArgs(message)
-            local r = FindPlayer(Args[2])
-            if r then
-                followingplr = r
-            else
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "Could Not Find Player!";
-                    Text = "";
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                return
-            end
-            getgenv().Following = true
-            game.StarterGui:SetCore("SendNotification",  {
-                Title = "Now Following";
-                Text = followingplr.Name;
-                Icon = "";
-                Duration = 5;
-                Callback = NotificationBindable;
-            })
-            task.spawn(function()
-                while true do task.wait()
-                    if not getgenv().Following then
-                        break
-                    end
-                    plr.Character:WaitForChild("HumanoidRootPart").CFrame = followingplr.Character:WaitForChild("HumanoidRootPart").CFrame
-                end
-            end)
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("now following " .. followingplr.Name,"All")
-        end
-        if string.sub(string.lower(unfollow), 0, #message) == string.lower(message) or string.find(message,unfollow) then
-            if getgenv().Following then
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "No Longer Following";
-                    Text = followingplr.Name;
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                getgenv().Following = false
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("no longer following " .. followingplr.Name,"All")
-            else
-                game.StarterGui:SetCore("SendNotification",  {
-                    Title = "Not Following Anyone";
-                    Text = "";
-                    Icon = "";
-                    Duration = 5;
-                    Callback = NotificationBindable;
-                })
-                game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("not following anyone","All")
-            end
-        end
-        if string.sub(string.lower(reset), 0, #message) == string.lower(message) or string.sub(string.lower(die), 0, #message) == string.lower(message) then
-            plr.Character.Humanoid.Health = 0
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("now dying","All")
-        end
-        if string.find(message,cmds) then
-            game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(".follow [plr] .unf .die .reset","All")
-        end
-	if string.find(message,lock) then
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("bot has been unlocked and now everyone can use it","All")
-		locked = false
+				SFBasePart(TRootPart)
+			end
+		elseif TRootPart and not THead then
+			SFBasePart(TRootPart)
+		elseif not TRootPart and THead then
+			SFBasePart(THead)
+		elseif not TRootPart and not THead and Accessory and Handle then
+			SFBasePart(Handle)
+		else
+			
+		end
+
+		BV:Destroy()
+		Humanoid:SetStateEnabled(Enum.HumanoidStateType.Seated, true)
+		workspace.CurrentCamera.CameraSubject = Humanoid
+
+		repeat
+			RootPart.CFrame = getgenv().OldPos * CFrame.new(0, .5, 0)
+			Character:SetPrimaryPartCFrame(getgenv().OldPos * CFrame.new(0, .5, 0))
+			Humanoid:ChangeState("GettingUp")
+			table.foreach(Character:GetChildren(), function(_, x)
+				if x:IsA("BasePart") then
+					x.Velocity, x.RotVelocity = Vector3.new(), Vector3.new()
+				end
+			end)
+			task.wait()
+		until (RootPart.Position - getgenv().OldPos.p).Magnitude < 25
+		workspace.FallenPartsDestroyHeight = getgenv().FPDH
+	else
 
 	end
-	end
-	end
-    end)
 end
-local GC = getconnections or get_signal_cons
-if GC then
-	for i,v in pairs(GC(game:GetService("Players").LocalPlayer.Idled)) do
-		if v["Disable"] then
-			v["Disable"](v)
-		elseif v["Disconnect"] then
-			v["Disconnect"](v)
+
+function chatMessage(str)
+	str = tostring(str)
+	if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+		TextChatService.TextChannels.RBXGeneral:SendAsync(str)
+	else
+		ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(str, "All")
+	end
+end
+
+if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
+	
+	TextChatService.MessageReceived:Connect(function(message)
+		local split = message.Text:split(" ")
+		if split[1] == 'fling' then
+			SkidFling(Players[split[2]])
 		end
-	end
-else
-	game:GetService("Players").LocalPlayer.Idled:Connect(function()
-		local VirtualUser = game:GetService("VirtualUser")
-		VirtualUser:CaptureController()
-		VirtualUser:ClickButton2(Vector2.new())
 	end)
+	
+else
+	
+	game.Players.LocalPlayer.Chatted:Connect(function(message)
+		local split = message:split(" ")
+		if split[1] == 'fling' then
+			SkidFling(Players[split[2]])
+		end
+	end)
+	
 end
-ANTIAFK = game.Players.LocalPlayer.Idled:connect(function()
-    game:FindService("VirtualUser"):Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-    task.wait(1)
-    game:FindService("VirtualUser"):Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
-game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Hi im a script bot basically just type .cmds to see all the commands!","All")
