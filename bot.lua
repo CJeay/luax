fc7aaf7aeb738841b937950591cda6f018d30dea
Notably <135164982+CJeay@users.noreply.github.com>
2024-06-10 21:57:41 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,47 +1,20 @@
 game.StarterGui:SetCore("SendNotification", {
-	Title = "Bot";
+	Title = "Swear Bot";
 	Text = "LMAO";
 	Duration = 3;
 })
 
 local Players = game:GetService('Players')
 
+local list = {
+	'FAG￰рuѕѕy',
+	'FAG￰nіggеr',
+	'FAG￰fuсk',
+	'FAG￰ѕhіt'
+}
+
 local TextChatService = game:GetService('TextChatService')
 local ReplicatedStorage = game:GetService('ReplicatedStorage')
-
-currentlyfollowing = nil
-
-task.spawn(function()
-	if currentlyfollowing then
-		local PathfindingService = game:GetService("PathfindingService")
-		local Path = PathfindingService:CreatePath({
-			AgentRadius = 3,
-			AgentHeight = 6,
-			AgentCanJump = true,
-			Costs = {
-				Water = 20
-			}
-		})
-
-		while true do
-			local playerRoot = currentlyfollowing.Character:FindFirstChild("HumanoidRootPart")
-				if playerRoot then
-					Path:ComputeAsync(script.Parent.HumanoidRootPart.Position, playerRoot.Position)
-					local Waypoints = Path:GetWaypoints()
-
-					for _, waypoint in pairs(Waypoints) do
-						if waypoint.Action == Enum.HumanoidStateType.Jumping then
-							script.Parent.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-						end
-						script.Parent.Humanoid:MoveTo(waypoint.Position)
-					end
-
-					script.Parent.Humanoid.MoveToFinished:Wait()
-				end
-			task.wait(1)
-		end
-	end
-end)
 
 function chatMessage(str)
 	str = tostring(str)
@@ -55,21 +28,19 @@ end
 if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
 	
 	TextChatService.MessageReceived:Connect(function(message)
-		local split = message.Text:split(" ")
-		if split[1] == '.follow' then
-			chatMessage('Following '..split[2])
-			currentlyfollowing = Players[split[2]]
+		if message == '.cuss' then
+			chatMessage(list[math.random(1,#list)])
 		end
 	end)
 	
 else
 	
-	game.Players.LocalPlayer.Chatted:Connect(function(message)
-		local split = message:split(" ")
-		if split[1] == '.follow' then
-			chatMessage('Following '..split[2])
-			currentlyfollowing = Players[split[2]]
-		end
-	end)
+	for i, v in pairs(Players:GetChildren()) do
+		v.Chatted:Connect(function(message)
+			if message == '.cuss' then
+				chatMessage(list[math.random(1,#list)])
+			end
+		end)
+	end
 	
 end
